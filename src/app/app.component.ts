f19965f4c4c3771c4c1ae09a919fdb1bc9431dfa
jeanmichelthompson <jmtokx@gmail.com>
2023-09-19 23:29:04 -0400

    added electron

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ClassDataService } from './class-data.service';
+import { SelectItem } from 'primeng/api/selectitem';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'turtleometer';
+  classOptions: SelectItem[] = [
+    { label: 'Default', value: 'Default' },
+  ];
+
+  constructor (private classDataService: ClassDataService) {}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -43,6 +44,7 @@ import { PaginatorModule } from 'primeng/paginator';
     ChipsModule,
     DynamicDialogModule,
     PaginatorModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
